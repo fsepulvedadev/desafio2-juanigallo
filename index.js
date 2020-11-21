a5c9074f0ebd7c3caf3394c0ea5a6877507c7989
Francisco <panchinrc1993@gmail.com>
2020-11-21 09:17:21 -0300

    loading timeout

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const gist = document.getElementById('gist');
 const repos = document.getElementById('repos');
 const url = 'https://api.github.com/users/panchixnrc';
 
-fetch(url)
-	.then((resp) => resp.json())
-	.then(function (data) {
-		nombre.innerText = data.name;
-		company.innerText = data.bio;
-		imagen.src = data.avatar_url;
-		gist.innerText = data.public_gists;
-		repos.innerText = data.public_repos;
-		followers.innerText = data.followers;
-	});
+window.setTimeout(function () {
+	fetch(url)
+		.then((resp) => resp.json())
+		.then(function (data) {
+			nombre.innerText = data.name;
+			company.innerText = data.bio;
+			imagen.src = data.avatar_url;
+			gist.innerText = data.public_gists;
+			repos.innerText = data.public_repos;
+			followers.innerText = data.followers;
+		});
+}, 1500);
