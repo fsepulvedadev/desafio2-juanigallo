78469cf5474d81d250cc0dc6a29ee99cd3c08c8c
Francisco <panchinrc1993@gmail.com>
2020-11-21 08:59:03 -0300

    desafio terminado

---
+++ index.js
@@ -0,0 +1,18 @@
+const imagen = document.getElementById('imagen');
+const nombre = document.getElementById('user');
+const company = document.getElementById('company');
+const followers = document.getElementById('followers');
+const gist = document.getElementById('gist');
+const repos = document.getElementById('repos');
+const url = 'https://api.github.com/users/panchixnrc';
+
+fetch(url)
+	.then((resp) => resp.json())
+	.then(function (data) {
+		nombre.innerText = data.name;
+		company.innerText = data.bio;
+		imagen.src = data.avatar_url;
+		gist.innerText = data.public_gists;
+		repos.innerText = data.public_repos;
+		followers.innerText = data.followers;
+	});
